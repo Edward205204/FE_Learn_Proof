06825102a64635926534480a43c6709b7bf30dd4
Edward205204 <nguyentminhkhoa1@gmail.com>
2026-03-02 15:22:37 +0700

    init project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,36 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import prettierConfig from 'eslint-config-prettier' // Import bản config xóa bỏ xung đột
+import prettierPlugin from 'eslint-plugin-prettier' // Import plugin để chạy prettier như 1 rule
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+  // Tích hợp Prettier
+  {
+    plugins: {
+      prettier: prettierPlugin
+    },
+    rules: {
+      'prettier/prettier': [
+        'warn',
+        {
+          arrowParens: 'always',
+          semi: false,
+          trailingComma: 'none',
+          tabWidth: 2,
+          endOfLine: 'auto',
+          useTabs: false,
+          singleQuote: true,
+          printWidth: 120,
+          jsxSingleQuote: true
+        }
+      ]
+    }
+  },
+  prettierConfig, // Quan trọng: Phải nằm cuối cùng
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts'])
+])
 
-export default eslintConfig;
+export default eslintConfig
